096e6a841aaa967a5df2b8ae2386c6085acafa1e
Jolene <harklllous1@gmail.com>
2024-10-15 19:21:07 +0100

    Initial commit

---
+++ src/main/java/net/jolene/jojosquared/JoJoSquared.java
@@ -0,0 +1,16 @@
+package net.jolene.jojosquared;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JoJoSquared implements ModInitializer {
+	public static final String MOD_ID = "jojosquared";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/jolene/jojosquared/JoJoSquaredClient.java
@@ -0,0 +1,10 @@
+package net.jolene.jojosquared;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JoJoSquaredClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
